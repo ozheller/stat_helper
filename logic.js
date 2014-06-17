5b6ee75cfbc6110365fc292ce49746370261e0b7
Oz Heller <oz@ozheller.com>
2014-06-17 07:55:09 -0500

    Update with changes

--- logic.js
+++ logic.js
@@ -1,34 +1,81 @@
-var att = new Object();
-  att.str = {
+var characterBuilder = {};
+characterBuilder.att = {};
+var att = ['str', 'dex', 'con', 'inte', 'wis', 'cha'];
+for(var x in att){
+	characterBuilder.att[att[x]] = {
+		value: 11,
+		mod: 0
+	}
+}
+
+/*
+
+
+
+characterBuilder.att = ['str', 'dex', 'con', 'inte', 'wis', 'cha'];
+  characterBuilder.att.str = {
     value: 10,
     mod: 0
   };
 
-  att.dex = {
+  characterBuilder.att.dex = {
     value: 10,
     mod: 0
   };
 
-  att.con = {
+  characterBuilder.att.con = {
     value: 10,
     mod: 0
   };
 
-  att.inte = {
+  characterBuilder.att.inte = {
     value: 10,
     mod: 0
   };
 
-  att.wis = {
+  characterBuilder.att.wis = {
     value: 10,
     mod: 0
   };
 
-  att.cha = {
+  characterBuilder.att.cha = {
     value: 10,
     mod: 0
   };
+*/
+characterBuilder.pp = 15;
 
-  att.pp = prompt("How many Power Points will be used?");
+characterBuilder.init=function(){
+	var pps = document.querySelector('.powerPoints');
+	pps.value=characterBuilder.pp;
+	pps.onkeyup=characterBuilder.ppChange;
+	pps.disabled=false;
+		// for (var x = 0, characterBuilder.att.lenght, x++){
+			// document.querySelector(this).innerHTML=this.value; 
+		// }
+	for(var x in characterBuilder.att){
+		console.log(x);
+		console.log(characterBuilder.att[x].value);
+		
+	}	
+};
 
-document.querySelector('.powerPoints').innerHTML="You have " + att.pp + " number of power points left.";
+characterBuilder.ppChange = function ppChange(e){
+	var pps = e.currentTarget;
+	console.log(e.currentTarget);
+	characterBuilder.pp=pps.value;
+	console.log(characterBuilder.pp);
+}
+
+characterBuilder.init();
+
+
+//document.querySelector('.str', '.dex', '.con', '.inte', '.wis', '.cha');
+
+
+
+	
+
+
+console.log(characterBuilder.att.str);
+console.log(characterBuilder.pp);
